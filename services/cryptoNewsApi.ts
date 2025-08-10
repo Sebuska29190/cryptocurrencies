634a939117efddc0b291ea1ff5a5909bc840c35f
Sebuska29190 <57123390+Sebuska29190@users.noreply.github.com>
2025-08-11 01:19:13 +0200

    Update cryptoNewsApi.ts

--- services/cryptoNewsApi.ts
+++ services/cryptoNewsApi.ts
@@ -1,13 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const rapidApiKey = process.env.NEXT_PUBLIC_RAPIDAPI_KEY;
-
 const cryptoNewsHeaders = {
   'X-RapidAPI-Key': rapidApiKey,
   'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com', // Poprawny Host z Twojego zrzutu ekranu
 };
 
-const baseUrl = 'https://crypto-news16.p.rapidapi.com'; // Poprawny bazowy URL
+const baseUrl = '/.netlify/functions/news-proxy'; // Poprawny bazowy URL
 
 const createRequest = (url: string) => ({ url, headers: cryptoNewsHeaders });
 
